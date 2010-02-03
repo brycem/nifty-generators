7b76fc325ba0211ccf8cd394c919a25ee5b5631f
Bryce McDonnell <bryce@bridgetownint.com>
2010-02-03 08:15:22 -0800

    more fine tuning to controller actions

--- rails_generators/nifty_scaffold/templates/tests/rspec/actions/destroy.rb
+++ rails_generators/nifty_scaffold/templates/tests/rspec/actions/destroy.rb
@@ -1,8 +1,8 @@
   describe "DESTROY" do
     
     it "destroy action should destroy model and redirect to index action" do
-      <%= singular_name %> = <%= class_name %>.first
-      delete :destroy, :id => <%= singular_name %>
+      valid_<%= singular_name %>
+      delete :destroy, :id => @<%= singular_name %>.id
       response.should redirect_to(<%= items_path('url') %>)
       <%= class_name %>.exists?(<%= singular_name %>.id).should be_false
     end
--- rails_generators/nifty_scaffold/templates/tests/rspec/actions/update.rb
+++ rails_generators/nifty_scaffold/templates/tests/rspec/actions/update.rb
@@ -6,7 +6,7 @@
     end
   
     it "update action should redirect when model is valid" do
-      valid_<%= class_name %>
+      valid_<%= singular_name %>
       put :update, :id => @<%= singular_name %>.id,  :<%= singular_name %> => Factory.attributes_for(:<%= singular_name %>)
       response.should redirect_to(<%= item_path_for_spec('url') %>)
     end
