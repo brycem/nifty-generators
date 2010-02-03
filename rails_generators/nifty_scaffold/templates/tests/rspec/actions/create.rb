f162526c0f356fb05c85fb0538c628867e13e226
Bryce McDonnell <bryce@bridgetownint.com>
2010-02-03 08:01:50 -0800

    cleaned up create, update and controller rspec

--- rails_generators/nifty_scaffold/templates/tests/rspec/actions/create.rb
+++ rails_generators/nifty_scaffold/templates/tests/rspec/actions/create.rb
@@ -1,6 +1,6 @@
   describe "CREATE" do
     it "create action should render new template when model is invalid" do
-      post :create, :<%= singular_name %> => Factory.attributes_for(:<%= singular_name %>, <%= attributes.first.name.to_sym %>   => nil)
+      post :create, :<%= singular_name %> => Factory.attributes_for(:<%= singular_name %>, :<%= attributes.first.name.to_sym %>   => nil)
       response.should render_template(:new)
     end
   
